bd62e2eb8164b76376e4f037232fad410c37273c
Yuliya-senkina <juliyasenkina@gmail.com>
2024-01-27 10:45:40 +0300

    second commit

--- src/main/java/page/KufarPage.java
+++ src/main/java/page/KufarPage.java
@@ -10,6 +10,7 @@ public class KufarPage {
     public String inputTextPassword = "//input[@id='password']";
     public String inputButtonForm = "//div[@data-name='login_submit']//button[@type='submit']";
     public String inputText = "//div[@class='styles_with_sms_block__6_xkj']//div//p";
+    public String textErrorEmail = "//div[@data-name='authorization_error_text']";
     WebDriver driver;
     public KufarPage(WebDriver driver) {
 
@@ -20,14 +21,23 @@ public class KufarPage {
         driver.findElement(By.xpath(inputButton)).click();
     }
 
-    public void sendKeysInputTextEmail() {
+    public void sendKeysInputTextEmail(String email) {
+
+        driver.findElement(By.xpath(inputTextEmail)).sendKeys(email);
+    }
+    public void sendKeysInputRandomTextEmail() {
 
         driver.findElement(By.xpath(inputTextEmail)).sendKeys(RandomUserData.getRandomCorrectEmail());
     }
 
-    public void sendKeysInputTextPassword() {
+    public void sendKeysInputRandomTextPassword() {
 
         driver.findElement(By.xpath(inputTextPassword)).sendKeys(RandomUserData.getRandomPassword());
+    }
+
+    public void sendKeysInputTextPassword(String password) {
+
+        driver.findElement(By.xpath(inputTextPassword)).sendKeys(password);
     }
 
     public void clickInputButtonForm() {
@@ -38,4 +48,10 @@ public class KufarPage {
 
         return driver.findElement(By.xpath(inputText)).getText();
     }
+
+    public String getTextErrorEmail() {
+
+        return driver.findElement(By.xpath(textErrorEmail)).getText();
+    }
+
 }
--- src/test/java/by/itacademy/senkina/ui/KufarLoginTest.java
+++ src/test/java/by/itacademy/senkina/ui/KufarLoginTest.java
@@ -7,15 +7,42 @@ import page.KufarPage;
 public class KufarLoginTest extends BaseTest {
 
     @Test
-    public void testKufarEmailPasswordClick() throws InterruptedException {
+    public void testLoginRandomEmailPassword() throws InterruptedException {
         driver.get("https://www.kufar.by/");
         KufarPage kufar = new KufarPage(driver);
         kufar.clickInputButton();
         Thread.sleep(1000);
-        kufar.sendKeysInputTextEmail();
-        kufar.sendKeysInputTextPassword();
+        kufar.sendKeysInputRandomTextEmail();
+        kufar.sendKeysInputRandomTextPassword();
         kufar.clickInputButtonForm();
         Thread.sleep(5000);
         Assertions.assertEquals("Введен неверный пароль либо такого профиля не существует", kufar.getTextError());
+    }
+
+    @Test
+    public void testLoginEmptyEmailPassword() throws InterruptedException {
+        driver.get("https://www.kufar.by/");
+        KufarPage kufar = new KufarPage(driver);
+        kufar.clickInputButton();
+        Thread.sleep(1000);
+        kufar.sendKeysInputTextEmail("");
+        kufar.sendKeysInputTextPassword("");
+        kufar.clickInputButtonForm();
+        Thread.sleep(5000);
+        Assertions.assertEquals("Заполните обязательное поле", kufar.getTextErrorEmail());
+        Assertions.assertEquals("Введите пароль", kufar.getTextErrorPassword());
+    }
+
+    @Test
+    public void testLoginEmptyEmailPassword2() throws InterruptedException {
+        driver.get("https://www.kufar.by/");
+        KufarPage kufar = new KufarPage(driver);
+        kufar.clickInputButton();
+        Thread.sleep(1000);
+        kufar.sendKeysInputTextEmail("");
+        kufar.sendKeysInputTextPassword("");
+        kufar.clickInputButtonForm();
+        Thread.sleep(5000);
+        Assertions.assertEquals("Заполните обязательное поле", kufar.getTextErrorEmail());
     }
 }
